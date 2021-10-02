2c27741d26409210587da84b86c48d3997c0f012
Josh Maggard <jmaggard62@gmail.com>
2021-10-02 13:45:34 -0500

    ball home created. Closes #7

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,46 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using ImportedTools;
+﻿using ImportedTools;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] GameObject ballPrefab;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    private bool _isClicking = false;
-
-    private Rigidbody2D _currentBall;
 
     public Color thrusterActiveColor;
     public Color thrusterInactiveColor;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            _isClicking = true;
-            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            pos.z = 0;
-            _currentBall = Instantiate(ballPrefab, pos, Quaternion.identity).GetComponent<Rigidbody2D>();
-            _currentBall.simulated = false;
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            var mouseUpPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouseUpPos.z = 0;
-            var diff = _currentBall.transform.position - mouseUpPos;
-            _currentBall.simulated = true;
-            _currentBall.velocity = diff.normalized * 40;
-            _currentBall = null;
-            _isClicking = false;
-        }
-    }
+   
 }
---
+++ Assets/ballHomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballHomeController : MonoBehaviour
+{
+    private float _stayCount = 0;
+    private bool _tamaIsInCollider = false;
+    public float StayToWinSeconds = 3;
+
+    private void Update()
+    {
+        if (_tamaIsInCollider)
+        {
+            _stayCount += Time.deltaTime;
+        }
+        else
+        {
+            _stayCount = 0;
+        }
+        
+        if(_stayCount >= StayToWinSeconds)
+            Debug.Log("YOU WIN");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Tama"))
+            return;
+        _tamaIsInCollider = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Tama"))
+            return;
+        _tamaIsInCollider = false;
+    }
+}
