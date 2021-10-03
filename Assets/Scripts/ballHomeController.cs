9e3ed15f3f07fefafb9ea4b9cc3c7fed6def9b85
Josh Maggard <jmaggard62@gmail.com>
2021-10-03 00:34:45 -0500

    home indicator. Closes #17

--- Assets/Scripts/ballHomeController.cs
+++ Assets/Scripts/ballHomeController.cs
@@ -8,17 +8,25 @@ public class ballHomeController : MonoBehaviour
     private float _stayCount = 0;
     private bool _tamaIsInCollider = false;
     public float StayToWinSeconds = 3;
+    public AnimationCurve showWinCurve;
+    [SerializeField] private SpriteRenderer winSpriteRend;
 
     private void Update()
     {
         if (_tamaIsInCollider)
         {
             _stayCount += Time.deltaTime;
+            Color winColor = Color.white;
+            var value = showWinCurve.Evaluate(_stayCount / StayToWinSeconds);
+            winColor.a = value;
+            winSpriteRend.color = winColor;
         }
         else
         {
             _stayCount = 0;
+            winSpriteRend.color = Color.clear;
         }
+        
         
         if(_stayCount >= StayToWinSeconds)
         {
